7d8cfefe11e0e28fa2211c1e7196dc18c38972ac
Решетникова Варвара Викторовна <vreshetnikova@misis.ru>
2026-07-23 14:08:27 +0300

    Setup PsychoPy environment

---
+++ stroop/participant.py
@@ -0,0 +1,58 @@
+"""
+participant.py
+
+Сбор информации об участнике.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime
+
+from psychopy import gui
+
+
+@dataclass
+class Participant:
+    """
+    Информация об участнике.
+    """
+
+    participant_id: str
+    age: str
+    gender: str
+    date: str
+
+
+def collect_participant_info() -> Participant:
+    """
+    Показывает диалоговое окно
+    и возвращает данные участника.
+    """
+
+    info = {
+        "ID участника": "",
+        "Возраст": "",
+        "Пол": "",
+    }
+
+    dialog = gui.DlgFromDict(
+        dictionary=info,
+        title="Информация об участнике"
+    )
+
+
+    if not dialog.OK:
+        raise SystemExit(
+            "Эксперимент отменен пользователем"
+        )
+
+
+    participant = Participant(
+        participant_id=info["ID участника"],
+        age=info["Возраст"],
+        gender=info["Пол"],
+        date=datetime.now().isoformat()
+    )
+
+    return participant
